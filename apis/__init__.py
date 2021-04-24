14c585700f398ddbc2a799c38d4754fd522f1c7b
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2021-04-24 17:42:14 -0300

    add the efficiency of system path

--- apis/__init__.py
+++ apis/__init__.py
@@ -2,7 +2,7 @@ from flask_restx import Api
 from .fuzzy_api import api as fuzzy
 from .energy_produced_api import api as energy
 from .efficiency_inversor_api import api as inversor_efficiency
-
+from .efficiency_system_api import api as system_efficiency
 api = Api(
     title='API de Processamento de Gatos',
     version='1.0',
@@ -13,3 +13,4 @@ api = Api(
 api.add_namespace(fuzzy, path='/catatau')
 api.add_namespace(energy, path='/produced')
 api.add_namespace(inversor_efficiency, path="/inversor")
+api.add_namespace(system_efficiency, path="/efficiency")
